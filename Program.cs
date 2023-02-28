7a7fd48a1f3451c0705d35a143c1516c7ecd4e58
Evgeniy Batishchev <121511841+EvgeniySpartanec@users.noreply.github.com>
2023-02-28 23:30:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами. 
+
+
+using System;
+
+
+
+string[] array = StringsArray();
+string[] result = FindValueArray(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+// просим пользователя задать значения для массива
+string[] StringsArray() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+// Проверяем каждое значение из массива
+int CountLess(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Возвращает новый массив заполненный 
+string[] FindValueArray(string[] input, int n) 
+{
+    string[] output = new string[CountLess(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
